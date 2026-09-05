ced5cbe31a28ee327bba3d7d2da8ab447ac5627d
lntaiko <79662198+lntaiko@users.noreply.github.com>
2026-09-06 01:38:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 // Pangkalan Data Senarai Berita
 let newsDatabase = [
